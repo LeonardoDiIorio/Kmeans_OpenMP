1c227c937f72fb6f0a3394a1697630ac1852574c
LeonardoDiIorio <125449504+LeonardoDiIorio@users.noreply.github.com>
2023-02-15 10:44:29 +0100

    Kmeans parallel implementation

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "Kmeans.cpp"
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include<omp.h>
+
+using namespace std;
+
+int main()
+{
+  int maxThreads=12;
+  int numTest=1;
+ 
+  ofstream myfile;
+  myfile.open ("example.csv");
+  for(int k=2;k<maxThreads+1;k+=2)
+  {
+    omp_set_num_threads(k);
+    int time=0;
+    Kmeans kmeansInstance(1000,10,280);
+    auto start = chrono::system_clock::now();
+    kmeansInstance.computeKmeans(40);
+    auto end = chrono::system_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::milliseconds>(end - start);
+    time += elapsed.count();
+    cout << "\t\tTime: " << time << endl;
+    myfile << time<<"\n";  
+  }
+  myfile.close();
+  return 0;
+}
